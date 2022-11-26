9825062395aeb4555360e2c355bafc74c383768b
mullen25312 <>
2022-11-26 15:12:14 +0100

    feat: Set up dev environment based on last year

---
+++ main.py
@@ -0,0 +1,27 @@
+# advent of code website: https://adventofcode.com/2022
+# github: https://github.com/mullen25312/aoc2022
+
+import os
+import importlib
+
+#dailyPuzzles = ["d00", "d01", "d02", "d03", "d04", "d05", "d06", "d07", "d08", "d09", "d10", "d11", "d12", "d13", "d14", "d17", "d20", "d21"]
+dailyPuzzles = ["d00"]
+# dailyPuzzles = dailyPuzzles[-2:]
+
+if __name__ == "__main__":
+
+    for module in dailyPuzzles:
+        # import daily module
+        importedModule = importlib.import_module(f"{module}.dailyPuzzle")
+        puzzle = importedModule.DailyPuzzle(os.path.join(module, "demo.txt"))
+
+        # solve puzzle
+        puzzle.parse()
+        puzzle.part_one()
+        puzzle.part_two()
+
+        # print results
+        print(f"####### {module} - results #######")
+        print(puzzle.part_one_result)
+        print(puzzle.part_two_result)
+        print(f"\n")
